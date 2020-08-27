dd1c2817799c285d4730742ca84e064a34f2f96c
Matheus Lima <matheus_sousa@protonmail.com>
2020-08-26 23:50:59 -0300

    app

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+    
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response.data);
+            setProjects(response.data);
+        })
+    }, []);
+    
+    return (
+        <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+        <SafeAreaView style={styles.container}>
+            
+        <FlatList 
+            data={projects}
+            keyExtractor={project => project.id}
+            renderItem={({item: project}) => (
+                <Text style={styles.project}>{project.title}</Text>
+            )}
+        />
+        </SafeAreaView>
+
+        {/* <View style={styles.container}>
+            {projects.map(project => (
+                <Text style={styles.project} key={project.id}>{project.title}</Text>
+            ))}
+        </View> */}
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+    },
+
+    project: {
+        color: '#FFF',
+        fontSize: 30,
+    },
+});
